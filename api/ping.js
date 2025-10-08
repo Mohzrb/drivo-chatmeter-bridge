0ae101d14911a317723bc8d0624cf688b59c5515
Mohzrb <mohamed@drivo.com>
2025-10-08 17:41:59 -0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, version: "ping-2025-10-07" });
+export default async function handler(req, res) {
+  return res.status(200).json({ ok: true, version: "ping-2025-10-07" });
 }
