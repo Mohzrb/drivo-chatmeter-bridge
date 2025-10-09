ca588e1eed20f6a9bc2d49479dc55516ae8b9624
Mohzrb <mohamed@drivo.com>
2025-10-09 10:05:02 -0400

    Update selftest.js

--- api/selftest.js
+++ api/selftest.js
@@ -1,13 +1,15 @@
+// api/selftest.js
 export default function handler(req, res) {
   res.status(200).json({
-    ok: true,
-    version: "selftest-2025-10-07",
-    have: {
+    node: process.version,
+    env: {
       ZENDESK_SUBDOMAIN: !!process.env.ZENDESK_SUBDOMAIN,
       ZENDESK_EMAIL: !!process.env.ZENDESK_EMAIL,
       ZENDESK_API_TOKEN: !!process.env.ZENDESK_API_TOKEN,
-      CHATMETER_V5_TOKEN: !!process.env.CHATMETER_V5_TOKEN,
-      SELF_BASE_URL: !!process.env.SELF_BASE_URL
+      ZD_FIELD_REVIEW_ID: !!process.env.ZD_FIELD_REVIEW_ID,
+      ZD_FIELD_LOCATION_ID: !!process.env.ZD_FIELD_LOCATION_ID,
+      ZD_FIELD_RATING: !!process.env.ZD_FIELD_RATING,
+      ZD_FIELD_FIRST_REPLY_SENT: !!process.env.ZD_FIELD_FIRST_REPLY_SENT
     }
   });
 }
