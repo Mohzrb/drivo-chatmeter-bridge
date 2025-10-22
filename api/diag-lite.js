2483664e2a803548dde250bfbfa71c22a3e5742a
Mohzrb <mohamed@drivo.com>
2025-10-22 04:40:53 -0400

    Fix presence object initialization syntax

--- api/diag-lite.js
+++ api/diag-lite.js
@@ -1,19 +1,17 @@
 export default async function handler(req, res) {
   try {
     const presence = {
-const presence = {
-  CHATMETER_V5_BASE: !!process.env.CHATMETER_V5_BASE,
-  CHATMETER_V5_TOKEN: !!process.env.CHATMETER_V5_TOKEN,
-  CHATMETER_TOKEN: !!process.env.CHATMETER_TOKEN,
-  CHATMETER_API_KEY: !!process.env.CHATMETER_API_KEY,
-  CHATMETER_USERNAME: !!process.env.CHATMETER_USERNAME,   // <-- add
-  CHATMETER_PASSWORD: !!process.env.CHATMETER_PASSWORD,   // <-- add
-  ZENDESK_SUBDOMAIN: !!process.env.ZENDESK_SUBDOMAIN,
-  ZENDESK_EMAIL: !!process.env.ZENDESK_EMAIL,
-  ZENDESK_API_TOKEN: !!process.env.ZENDESK_API_TOKEN,
-  CRON_SECRET: !!process.env.CRON_SECRET
-};
-
+      CHATMETER_V5_BASE: !!process.env.CHATMETER_V5_BASE,
+      CHATMETER_V5_TOKEN: !!process.env.CHATMETER_V5_TOKEN,
+      CHATMETER_TOKEN: !!process.env.CHATMETER_TOKEN,
+      CHATMETER_API_KEY: !!process.env.CHATMETER_API_KEY,
+      CHATMETER_USERNAME: !!process.env.CHATMETER_USERNAME,
+      CHATMETER_PASSWORD: !!process.env.CHATMETER_PASSWORD,
+      ZENDESK_SUBDOMAIN: !!process.env.ZENDESK_SUBDOMAIN,
+      ZENDESK_EMAIL: !!process.env.ZENDESK_EMAIL,
+      ZENDESK_API_TOKEN: !!process.env.ZENDESK_API_TOKEN,
+      CRON_SECRET: !!process.env.CRON_SECRET
+    };
 
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
